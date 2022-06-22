5faf39558e724ee1b71d4a5e1e18264692de2882
MandyMull <mandymullenberg@hotmail.com>
2022-06-22 12:54:35 +0200

    assignment 1

--- functions.js
+++ functions.js
@@ -14,6 +14,14 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function getSchool() {
+  return "Novi Hogeschool";
+}
+
+const highSchool = getSchool()
+
+console.log(highSchool);
+
 
 
 /* Opdracht 2 */
